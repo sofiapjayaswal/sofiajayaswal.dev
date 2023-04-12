d8f8b161806b7b60c1a2375346f4f4219a165dee
Sofia Jayaswal <sofia.p.jayaswal.25@dartmouth.edu>
2023-04-12 12:42:57 -0400

    added more plugins for EC

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import autoprefixer from 'autoprefixer';
-
+import faviconsPlugin from 'vite-plugin-favicon';
+import tailwindPlugin from 'vite-plugin-tailwind';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     eslint(),
+    faviconsPlugin(),
+    tailwindPlugin(),
   ],
   css: {
     postcss: {
