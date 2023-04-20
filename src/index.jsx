159c1e72c90e0fbe686bfe9a46fbb3cd5f941189
Sofia Jayaswal <sofia.p.jayaswal.25@dartmouth.edu>
2023-04-19 20:57:48 -0400

    added react router

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,55 @@
-/* eslint-disable react/function-component-definition */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route, NavLink, useParams,
+} from 'react-router-dom';
 import './style.scss';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+function App(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+
+function Welcome(props) {
+  return <div>Welcome</div>;
+}
+
+function Nav(props) {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+
+function FallBack(props) {
+  return <div>URL Not Found</div>;
+}
 
 const root = createRoot(document.getElementById('main'));
 root.render(<App />);
