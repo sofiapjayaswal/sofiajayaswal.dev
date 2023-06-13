f800b39566d838628da41fb0fdf91b6d60bf79cd
Sofia Jayaswal <sofia.p.jayaswal.25@dartmouth.edu>
2023-06-13 13:45:00 +0200

    changed scroll styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,13 +8,13 @@ function Footer() {
     <div id="footer">
       <h2 className="footer-text">Sofia Jayaswal 2023</h2>
       <div id="links">
-        <a href="mailto:sofiajayaswal@gmail.com">
+        <a className="button-link" href="mailto:sofiajayaswal@gmail.com">
           <img src={emailIcon} alt="" />
         </a>
-        <a target="_blank" href="https://github.com/sofiapjayaswal" rel="noreferrer">
+        <a className="button-link" target="_blank" href="https://github.com/sofiapjayaswal" rel="noreferrer">
           <img src={gitIcon} alt="" />
         </a>
-        <a target="_blank" href="https://www.linkedin.com/in/sofiajayaswal/" rel="noreferrer">
+        <a className="button-link" target="_blank" href="https://www.linkedin.com/in/sofiajayaswal/" rel="noreferrer">
           <img src={linkedinIcon} alt="" />
         </a>
       </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,15 +5,17 @@ import logo from '../img/logo.png';
 function Header() {
   return (
     <div className="nav-bar">
-      <img src={logo} alt="" />
+      <Link activeClass="item-active" to="home" spy smooth offset={-150} duration={500}>
+        <img src={logo} alt="" />
+      </Link>
       <div className="nav-links">
-        <Link activeClass="item-active" to="home" spy smooth duration={500}>
+        <Link activeClass="item-active" to="home" spy smooth offset={-150} duration={500}>
           <div className="nav-item">Home</div>
         </Link>
-        <Link activeClass="item-active" to="about" spy smooth offset={-100} duration={500}>
+        <Link activeClass="item-active" to="about" spy smooth offset={-150} duration={500}>
           <div className="nav-item">About</div>
         </Link>
-        <Link activeClass="item-active" to="experience" spy smooth offset={-70} duration={500}>
+        <Link activeClass="item-active" to="experience" spy smooth offset={-150} duration={500}>
           <div className="nav-item">Experience</div>
         </Link>
         {/* <Link activeClass="itemActive" to="projects" spy={true} smooth={true} offset={-70} duration={500} style={{color: 'FFFFFF'}}>
