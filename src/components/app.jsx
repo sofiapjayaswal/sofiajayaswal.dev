f1521854e8e5aadb3f59751240152fda9ae5c0c5
Sofia Jayaswal <sofia.p.jayaswal.25@dartmouth.edu>
2023-06-14 20:31:51 +0200

    added animation on render

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import About from './about';
 import Experience from './experience';
 import Footer from './footer';
@@ -9,12 +10,19 @@ function App() {
   return (
     <div id="main">
       <Header />
-      <div id="content">
-        <Home />
-        <About />
-        <Experience />
-        <Footer />
-      </div>
+      <motion.div
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.4 }}
+      >
+        <div id="content">
+          <Home />
+          <About />
+          <Experience />
+          <Footer />
+        </div>
+      </motion.div>
     </div>
   );
 }
